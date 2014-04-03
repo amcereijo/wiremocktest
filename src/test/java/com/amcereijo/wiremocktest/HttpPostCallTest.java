9553184c3dfed44b7f3d8c510df6a6a9e901b69f
amcereijo <amcereijo@gmail.com>
2014-04-03 09:56:17 +0200

    Adaptamos la nueva forma de usar WireMockClassRule

--- src/test/java/com/amcereijo/wiremocktest/HttpPostCallTest.java
+++ src/test/java/com/amcereijo/wiremocktest/HttpPostCallTest.java
@@ -5,6 +5,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 
 import org.junit.Assert;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,13 +16,15 @@ import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 @RunWith(JUnit4.class)
 public class HttpPostCallTest {
 
+	@ClassRule
+	public static WireMockClassRule wireMockRule = new WireMockClassRule(HttpPostCall.PORT);
 	@Rule
-	public WireMockClassRule wireMockRule = new WireMockClassRule(HttpPostCall.PORT);
+	public WireMockClassRule instanceRule = wireMockRule;
 	
 	@Test
 	public void testPost(){ 
 		String responseText = "Some content";
-		wireMockRule.stubFor(get(urlEqualTo(HttpPostCall.URL))
+		instanceRule.stubFor(get(urlEqualTo(HttpPostCall.URL))
 	            .willReturn(aResponse()
 	                .withStatus(200)
 	                .withHeader("Content-Type", "application/json")
